96c21e2910d536165f587aa5ac7b5fd47c0a1202
Alouiscious Sims <info@reneesoul.com>
2019-06-25 06:42:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,6 +3,16 @@
 def countdown(input)
   while input > 0
     puts "#{input} SECOND(S)!"
+    input.to_i -= 1
+
+  end
+
+end
+
+# same countdown with a one second timer
+def countdown_with_sleep(input)
+  while input > 0
+    puts "#{input} SECOND(S)!"
     sleep(1.0)
     input.to_i -= 1
 
